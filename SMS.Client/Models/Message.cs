95b5bd988030e8a9bcbba5b4f30010ad7886b77d
MaRioBoT <mario.botero@cafexport.com>
2015-08-27 18:48:40 -0500

    first protection

---
+++ SMS.Client/Models/Message.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SMS.Client.Models
+{
+    public class MessageToSend
+    {
+        public string to { get; set; }
+        public string from { get; set; }
+        public string body { get; set; }
+        public string MediaURL { get; set; }
+    }
+}
